fe63147ce3579604303b4726049623a9b40c4b53
Halo <44603549+HaloKim@users.noreply.github.com>
2021-08-13 18:23:23 +0900

    Update day10.py

--- 1d1c/day10.py
+++ 1d1c/day10.py
@@ -18,26 +18,23 @@ for i in range(len(data)):
 '''
 
 # 14501 모르겠다
-day = int(input())
-money = []
-next = 0
-total = 0
-start = 0
-maximum = []
-for _ in range(day):
-    money.append(input().split())
-def temp(money, start, total, maximum):
-    next = start
-    next += int(money[start][0])
-    if next >= day:
-        maximum.append(total)
-        print(maximum)
-        return
-    total += int(money[start][1])
-    print(money[start])
-    temp(money, next, total, maximum)
+n = int(input())
+day = []
+cost = []
+dp=[]
 
-for i in range(day):
-    print(i)
-    temp(money, i, total, maximum)
-print(max(maximum))
+for i in range(n):
+  x,y = map(int, input().split())
+  day.append(x)
+  cost.append(y)
+  dp.append(y)
+dp.append(0)  #뒤에 0을 추가해서 인덱스초과 오류 방지
+
+#뒤에서부터 확인
+for i in range(n-1, -1, -1):
+  if day[i] + i > n: #데드라인이 기한을 넘어가는경우
+    dp[i] = dp[i+1]
+  else:
+    dp[i] = max(dp[i+1], cost[i]+dp[i+day[i]])
+
+print(dp[0])
