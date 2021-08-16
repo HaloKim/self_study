120e38ac805d586a9c7be8486ab1a0b09f748496
Halo <44603549+HaloKim@users.noreply.github.com>
2021-08-16 21:19:46 +0900

    day12

---
+++ 1d1c/day12.py
@@ -0,0 +1,17 @@
+# 4811
+arr = []
+def pill(n, arr):
+    arr = [[0]*(n+1) for _ in range(n+1)]
+    for i in range(n+1):
+        for j in range(1,n+1):
+            if i == 0:
+                arr[i][j] = 1
+            elif i <= j:
+                arr[i][j] = arr[i-1][j] + arr[i][j-1]
+    print(max(arr[-1]))
+while(True):
+    n = int(input())
+    if n == 0:
+        break
+    pill(n, arr)
+    
