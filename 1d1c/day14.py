6af4214dae5c1755223f0ac88047c2666e87e2e9
Halo <44603549+HaloKim@users.noreply.github.com>
2021-08-18 19:19:52 +0900

    Create day14.py

---
+++ 1d1c/day14.py
@@ -0,0 +1,28 @@
+# 10844 설계실패
+'''
+# 첫시도 알고리즘자체가 틀렸나보다. 후반에 틀림
+n = int(input())
+dp = [[0]*(10) for _ in range(n+1)]
+
+for i in range(1,n+1):
+    for j in range(1,10):
+        if i == 1:
+            dp[i][j] = j
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+print(max(dp[-1]) % 1000000000 )
+'''
+n = int(input())
+dp = [[0]*(10) for _ in range(n+1)]
+
+for i in range(1,n+1):
+    for j in range(10):
+        if i == 1 and j > 0:
+            dp[i][j] = 1
+        elif j==9:
+            dp[i][j] = dp[i-1][j-1]
+        elif j==0:
+            dp[i][j] = dp[i-1][j+1]
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+print(sum(dp[-1]) % 1000000000)
