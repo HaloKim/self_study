5cd775551535e9d80b0720d9bb838523116c291f
Halo <44603549+HaloKim@users.noreply.github.com>
2021-08-19 20:27:27 +0900

    Update day15.py

--- 1d1c/day15.py
+++ 1d1c/day15.py
@@ -1,3 +1,29 @@
+# 1463
+''' 시간초과 ..
+n = int(input())
+count = 0
+def temp(n):
+    global count
+    arr = [n]
+    if (n-1) % 3 == 0:
+        count += 1
+        arr[0] = n-1
+    if arr[0]%3 == 0 and arr[0] >= 3:
+        count += 1
+        arr.append(arr[0]//3)
+    elif arr[0]%2 == 0 and arr[0] >= 2:
+        count += 1
+        arr.append(arr[0]//2)
+    # print(arr)
+    return arr
+while(True):
+    if n == 1:
+        print(count)
+        break
+    tmp = temp(n)
+    n = min(tmp)
+'''
+
 def calc(l):
     global ans
     ans+=1
@@ -8,7 +34,6 @@ def calc(l):
             nl.append(a/3)
         if a%2==0 and a>=2:
             nl.append(a/2)
-        print(nl)
     return nl
 
 n=int(input())
