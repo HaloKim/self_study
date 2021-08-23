d89d999c261dfafaf76137772eb46083f6d7b183
Halo <44603549+HaloKim@users.noreply.github.com>
2021-08-23 19:21:20 +0900

    Create day19.py

---
+++ 1d1c/day19.py
@@ -0,0 +1,23 @@
+# 2839
+n = int(input())
+count = 0
+while(True):
+    if n == 0:
+        break
+    if n >= 15:
+        n = n - 5
+        count += 1
+    elif n%5 == 0 and n >= 5:
+        count += n//5
+        n = n - (n//5)*5
+    elif n%3 == 0:
+        count += n//3
+        n = n - (n//3)*3
+    elif n >= 3:
+        n = n - 3
+        count += 1
+    else:
+        count = -1
+        break
+    print(n)
+print(count)
