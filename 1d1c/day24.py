331be9e42ffb62a818e5ec5cbb4bef9d90ffc68d
Halo <44603549+HaloKim@users.noreply.github.com>
2021-08-29 19:23:56 +0900

    Create day24.py

---
+++ 1d1c/day24.py
@@ -0,0 +1,12 @@
+# 1149
+n = int(input())
+house = []
+for _ in range(n):
+    house.append(list(map(int, input().split())))
+
+for i in range(1,n):
+    house[i][0] += min(house[i-1][1], house[i-1][2])
+    house[i][1] += min(house[i-1][0], house[i-1][2])
+    house[i][2] += min(house[i-1][0], house[i-1][1])
+
+print(min(house[n-1]))
