3d99c09bbfe11112e52831b2e0761042dbe14bfa
Halo <44603549+HaloKim@users.noreply.github.com>
2021-09-03 20:24:05 +0900

    day29

--- 1d1c/day29.py
+++ 1d1c/day29.py
@@ -1,12 +1,29 @@
-# 1922
+# 1922 크루스칼
 n = int(input())
 m = int(input())
 arr = []
 for i in range(m):
     arr.append(list(map(int, input().split())))
 arr.sort(key=lambda x: x[-1])
-def bfs(a):
-    queue = []
-    queue.append([x,y])
-    visit = [[0]*2 for i in range(m)]
-bfs()
+print(arr)
+cycle_table = [i for i in range(n + 1)]
+
+def union(cycle_table, x, y):
+    x = find(x)
+    y = find(y)
+    if x < y:
+        cycle_table[y] = x
+    else:
+        cycle_table[x] = y
+
+def find(a):
+    if cycle_table[a] != a:
+        return find(cycle_table[a])
+    return cycle_table[a]
+
+result = 0
+for x,y,c in arr:
+    if find(x) != find(y):
+        result += c
+        union(cycle_table,x,y)
+print(result)
