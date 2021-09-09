dc87ed597034b7545c1e9cf63fe7eff6fd10efaa
Halo <44603549+HaloKim@users.noreply.github.com>
2021-09-09 23:01:50 +0900

    day35

--- 1d1c/day35.py
+++ 1d1c/day35.py
@@ -1,5 +1,7 @@
+# 17142
 import sys
 from collections import deque
+from itertools import combinations
 n,m = map(int, sys.stdin.readline().split())
 mat = [[0]*n for _ in range(n)]
 for i in range(n):
@@ -17,16 +19,23 @@ def bfs(a,b):
             a += mov_x[i]
             b += mov_y[i]
             if a < 0 and b < 0 and a >= n and b >= n:
-                continue
-            elif wall[a][b] != 1:
-                visit[a][b] += 1        
-
+                continue    
 virus = []
-wall = []
-active = []
+active = 0
+time = 0
+cnt = 0
+
+def dfs(cnt):
+    if cnt == m:
+        bfs()
+        return
+    else:
+        for i in range(len(virus)):
+            dfs(cnt+1)
+        
 for i in range(n):
     for j in range(n):
         if mat[i][j] == 2:
             virus.append([i,j])
-        if mat[i][j] == 1:
-            wall.append([i,j])
+        if mat[i][j] == 0:
+            active += 1
