fb1f2d4cfa4a479e925daf66baaff718d677b193
Halo <44603549+HaloKim@users.noreply.github.com>
2021-09-20 16:59:20 +0900

    46

--- 1d1c/day46.py
+++ 1d1c/day46.py
@@ -1,37 +1,32 @@
+# 1707
 import sys
 from collections import deque
 
 k = int(sys.stdin.readline())
-
-def bfs(q):
-    visit = [0]*(v+1)
-
-    while q:
-        x,y = q.popleft()
-
-        if visit[x] == 0:
-            visit[x] = 1
-        else:
-            visit[x] = visit[x]
-
-        if visit[y] == 0:
-            visit[y] = visit[x]*(-1)
-        else:
-            visit[y] = visit[y]
-
-        if visit[x] == visit[y]:
-            return 0
-    return 1
-
+    
 for _ in range(k):
     v,e = map(int, sys.stdin.readline().strip().split())
-    q = deque()
-    q2 = deque()
+    graph = [[] for _ in range(v+1)]
+    visit = [0]*(v+1)
     for i in range(e):
         a,b = map(int, sys.stdin.readline().strip().split())
-        q.append([a,b])
-        q2.append([b,a])
-    if bfs(q)+bfs(q2) >= 1:
-        print("YES")
+        graph[a].append(b)
+        graph[b].append(a)
+    flag = 1
+    q = deque()
+    for i in range(1,v+1):
+        if visit[i] == 0:
+            q.append(i)
+            visit[i] = 1
+            while q:
+                dot = q.popleft()
+                for j in graph[dot]:
+                    if visit[j] == 0:
+                        q.append(j)
+                        visit[j] = -1 * visit[dot]
+                    elif visit[j] == visit[dot]:
+                        flag = 0
+    if flag == 0:
+        print('NO')
     else:
-        print("NO")
+        print('YES')
