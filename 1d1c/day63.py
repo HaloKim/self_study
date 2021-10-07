0e135bb3d2966f485c13e3d4a9edd765ae81efac
Halo <44603549+HaloKim@users.noreply.github.com>
2021-10-07 18:47:05 +0900

    63

--- 1d1c/day63.py
+++ 1d1c/day63.py
@@ -1,31 +1,49 @@
 # 14499
-N,M,x,y,k = map(int,input().split())
-mapp = [list(map(int,input().split())) for _ in range(N)]
-order = list(map(int,input().split()))
-dice = [0]*6
-d = ((1,0),(0,1),(0,-1),(-1,0))
+# N,M,x,y,k = map(int,input().split())
+# mapp = [list(map(int,input().split())) for _ in range(N)]
+# order = list(map(int,input().split()))
+# dice = [0]*6
+# d = ((1,0),(0,1),(0,-1),(-1,0))
 
-def change(w):
-    if w == 1:
-        dice[0],dice[2],dice[4],dice[5] = dice[5],dice[4],dice[0],dice[2]
-    if w == 2:
-        dice[0],dice[2],dice[4],dice[5] = dice[4],dice[5],dice[2],dice[0]
-    if w == 3:
-        dice[0],dice[1],dice[2],dice[3] = dice[3],dice[0],dice[1],dice[2]
-    if w == 4:
-        dice[0],dice[1],dice[2],dice[3] = dice[1],dice[2],dice[3],dice[0]
+# def change(w):
+#     if w == 1:
+#         dice[0],dice[2],dice[4],dice[5] = dice[5],dice[4],dice[0],dice[2]
+#     if w == 2:
+#         dice[0],dice[2],dice[4],dice[5] = dice[4],dice[5],dice[2],dice[0]
+#     if w == 3:
+#         dice[0],dice[1],dice[2],dice[3] = dice[3],dice[0],dice[1],dice[2]
+#     if w == 4:
+#         dice[0],dice[1],dice[2],dice[3] = dice[1],dice[2],dice[3],dice[0]
 
-for w in range(k):
-    dx,dy = d[order[w]%4]
-    nx = x + dx
-    ny = y + dy
-    if not 0 <= nx < N or not 0 <= ny < M:
-        continue
-    change(order[w])
-    if mapp[nx][ny] == 0:
-        mapp[nx][ny] = dice[0]
+# for w in range(k):
+#     dx,dy = d[order[w]%4]
+#     nx = x + dx
+#     ny = y + dy
+#     if not 0 <= nx < N or not 0 <= ny < M:
+#         continue
+#     change(order[w])
+#     if mapp[nx][ny] == 0:
+#         mapp[nx][ny] = dice[0]
+#     else:
+#         dice[0] = mapp[nx][ny]
+#         mapp[nx][ny] = 0
+#     print(dice[2])
+#     x,y = nx,ny
+
+# 14501
+n = int(input())
+dp = [0]*(n+1)
+t = []
+p = []
+
+for _ in range(n):
+    a,b = map(int,input().split())
+    t.append(a)
+    p.append(b)
+
+for i in range(n-1,-1,-1):
+    if i + t[i] > n:
+        dp[i] = dp[i+1]
     else:
-        dice[0] = mapp[nx][ny]
-        mapp[nx][ny] = 0
-    print(dice[2])
-    x,y = nx,ny
+        dp[i] = max(dp[i+1], p[i] + dp[i + t[i]])
+print(max(dp))
